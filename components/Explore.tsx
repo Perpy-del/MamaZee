437e01c639cafea1fd6776bd89cc541e10f0ede7
Perpy-del <perpetualmeninwa@gmail.com>
2024-01-12 17:45:52 +0100

    feat: add shop in explore section

--- components/Explore.tsx
+++ components/Explore.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import ExploreShop from './ExploreSection/ExploreShop'
+import ExploreTreatment from './ExploreSection/ExploreTreatment'
 
 type Props = {}
 
 const Explore = (props: Props) => {
   return (
-    <div className='px-[80px] pt-[50px] pb-[80px]'>Explore</div>
+    <div className='px-[80px] pt-[50px] pb-[80px]'>
+      <ExploreShop />
+      <ExploreTreatment />
+    </div>
   )
 }
 
--- components/ExploreSection/ExploreShop.tsx
+++ components/ExploreSection/ExploreShop.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { exploreCards } from './data';
+import Image from 'next/image';
+import { Button } from '../ui/button';
 
-type Props = {}
+type Props = {};
 
 const ExploreShop = (props: Props) => {
   return (
-    <div>ExploreShop</div>
-  )
-}
+    <div>
+      <div className='pb-[60px] text-center'>
+        <h1 className='text-[30px] font-bold'>Explore our store</h1>
+        <h4>Hair products crafted for the best hair results</h4>
+      </div>
+      <div className='flex gap-[20px]'>
+        {exploreCards.map((card, index) => {
+          return (
+            <div key={index} className='flex flex-col'>
+              <div className='w-[270px] h-[400px]'>
+              <Image
+                src={card.cardImage}
+                alt="product_cards"
+                width={288}
+                height={400}
+                className='w-fit object-cover'
+              />
+              </div>
+              <div className='flex flex-col gap-[10px] text-center'>
+                <h2 className='uppercase text-[18px] font-bold'>{card.product}</h2>
+                <h3 className='text-[14px]'>{card.prodDescription}</h3>
+                <Button variant='mzvariant' className='rounded'>{card.prodButton}</Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default ExploreShop
+export default ExploreShop;
