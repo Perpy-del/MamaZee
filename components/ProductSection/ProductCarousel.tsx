32f90a357cfde369e9a116c679edcc5a8fcef092
Perpy-del <perpetualmeninwa@gmail.com>
2024-01-16 18:54:41 +0100

    feat: product carousel

--- components/ProductSection/ProductCarousel.tsx
+++ components/ProductSection/ProductCarousel.tsx
@@ -1,17 +1,88 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+'use client';
+import React from 'react';
+import { productCarousel } from './data';
+import { Button } from '../ui/button';
 
-type Props = {}
+type Props = {};
 
 const ProductCarousel = (props: Props) => {
-  return (
-    <div>
-      <div>
-        <h1>Luxury and affordable</h1>
-        <h3>Hair products crafted for the best hair results</h3>
-      </div>
-      <div></div>
-    </div>
-  )
-}
+  const [currentIndex, setCurrentIndex] = React.useState<number>(0);
 
-export default ProductCarousel
+  const showImage = (index: number) => {
+    setCurrentIndex(index);
+    clearInterval(intervalIdRef.current); // Clear the interval when a button is clicked
+  };
+
+  React.useEffect(() => {
+    startAutoSwitching();
+    return () => {
+      clearInterval(intervalIdRef.current); // Cleanup on component unmount
+    };
+  }, []);
+
+  const intervalIdRef = React.useRef<any>();
+  const startAutoSwitching = () => {
+    intervalIdRef.current = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % productCarousel.length);
+    }, 5000);
+  };
+
+  return (
+    <div className="text-mzLight pt-[100px]">
+      <div className="pb-[60px] text-center">
+        <h1 className="font-[600] text-[30px] sm:text-[25px]">
+          Luxury and affordable
+        </h1>
+        <h3 className="text-mzProdLight">
+          Hair products crafted for the best hair results
+        </h3>
+      </div>
+      <div className="flex flex-col gap-5 pb-[20px] px-[30px] items-center">
+        <div className="flex">
+          {productCarousel.map((product, index) => {
+            return (
+              <div key={index} className={`${currentIndex === index ? 'flex' : 'hidden'} justify-center h-full`}>
+                <div className="w-[45%]">
+                  <img src={product.productImage} alt="Image" />
+                </div>
+                <div className="w-[45%] bg-mzLight px-[40px] py-[50px] flex flex-col gap-5 rounded-r">
+                  <h1 className="text-mzBlack text-[25px] font-[600]">
+                    {product.productName}
+                  </h1>
+                  <h3 className="text-mzTextBlack text-[14px]">
+                    {product.productDesc}
+                  </h3>
+                  <Button
+                    variant="mzvariant"
+                    size="mzsize"
+                    className="mt-[40px]"
+                  >
+                    Shop now
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex gap-2">
+          {productCarousel.map((_, index) => {
+            return (
+              <div
+                key={index}
+                className={`w-2 h-2 ${
+                  currentIndex === index ? 'bg-mzGold' : 'bg-mzLight'
+                } rounded-full cursor-pointer`}
+                onClick={() => {
+                  showImage(index);
+                }}
+              ></div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCarousel;
--- components/ProductSection/data.ts
+++ components/ProductSection/data.ts
@@ -20,3 +20,21 @@ export const productData = [
         productAmount: 'NGN 22,100'
     }
 ]
+
+export const productCarousel = [
+    {
+        productImage: '/products/product_carousel.svg',
+        productName: "Mamazee Hair Growth Oil",
+        productDesc: "Achieve that sought-after natural glow without worrying about breakage by incorporating this one-of-a-kind hair oil into your routine. Its remarkable feature lies in its ability to impart a smooth and relaxing feel to your hair, leaving you yearning for more. Witness not only rapid but also impressive hair growth, starting with just a few inches. With continued use, you'll be amazed at the hair growth you never thought possible. Even if you're interested in turning your relaxed hair into it's natural state without trimming your hair, Mamazee's got you covered.",
+    },
+    {
+        productImage: '/products/product_carousel.svg',
+        productName: "Mamazee Hair Gelatin",
+        productDesc: "Achieve that sought-after natural glow without worrying about breakage by incorporating this one-of-a-kind hair oil into your routine. Its remarkable feature lies in its ability to impart a smooth and relaxing feel to your hair, leaving you yearning for more. Witness not only rapid but also impressive hair growth, starting with just a few inches. With continued use, you'll be amazed at the hair growth you never thought possible. Even if you're interested in turning your relaxed hair into it's natural state without trimming your hair, Mamazee's got you covered.",
+    },
+    {
+        productImage: '/products/product_carousel.svg',
+        productName: "Mamazee Hair Shampoo",
+        productDesc: "Achieve that sought-after natural glow without worrying about breakage by incorporating this one-of-a-kind hair oil into your routine. Its remarkable feature lies in its ability to impart a smooth and relaxing feel to your hair, leaving you yearning for more. Witness not only rapid but also impressive hair growth, starting with just a few inches. With continued use, you'll be amazed at the hair growth you never thought possible. Even if you're interested in turning your relaxed hair into it's natural state without trimming your hair, Mamazee's got you covered.",
+    },
+]
