e9f581f119e6b3296a28bf9358d7afd7d0d1884c
Perpy-del <perpetualmeninwa@gmail.com>
2024-02-13 13:28:48 +0100

    feat: add a scroll button for easy navigation

---
+++ components/ScrollButton.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { ArrowUp } from 'lucide-react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { Button } from '@/components/ui/button';
+
+type Props = {};
+
+const ScrollButton = (props: Props) => {
+  const [showScrollButton, setShowScrollButton] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const section = document.getElementById('explore');
+      if (section?.offsetTop && scrollPosition > section?.offsetTop) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  function scrollToTop() {
+    const scrollStep = -window.scrollY / (400 / 15); // Change 500 for desired duration
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 15);
+  }
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="mzvariant"
+            onClick={scrollToTop}
+            className={`${
+              showScrollButton
+                ? 'rounded-full w-10 h-10 p-0 fixed bottom-5 right-5 z-[99]'
+                : 'hidden'
+            } `}
+          >
+            <ArrowUp size={20} />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add to library</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default ScrollButton;
